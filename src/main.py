11b5889fde660229e4a23f8025d78a6ca125cdb4
Hamzah Imran <hamzah@Hamzahs-MacBook-Air.local>
2024-10-21 02:47:28 -0400

    initial data cleaning, snowflake data warehouse pending

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import datasetDownloadScript as datasetDownloadScript
+import inspectData as inspectData
+import cleanData as cleanData
+
+df = pd.read_csv('../data/drinks.csv') #this reads the csv file and saves it in a pandas dataframe 
+
+keepIn = True
+
+while keepIn:
+
+    print("Press 1 to download the dataset")
+    print("Press 2 to inspect the dataset")
+    print("Press 3 to clean the dataset")
+    print("Press 4 to exit")
+    print("Enter your choice: ")
+    inputValue = input()
+
+    if inputValue == '1':
+        datasetDownloadScript.datasetDownload()
+
+    elif inputValue == '2':
+        inspectData.inspectData(df)
+
+    elif inputValue == '3':
+        df = cleanData.cleanData(df)
+
+    elif inputValue == '4':
+        keepIn = False
+
+
+
