6a115df41fa5702edaebf8ab4e894774a679e453
Hamzah Imran <hamzah@Hamzahs-MacBook-Air.local>
2024-10-21 20:17:43 -0400

    Snowflake credentials hidden, ETL data pipeline completed

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datasetDownloadScript as datasetDownloadScript
 import inspectData as inspectData
 import cleanData as cleanData
+import connectToSnowflake as connectToSnowflake
 
 df = pd.read_csv('../data/drinks.csv') #this reads the csv file and saves it in a pandas dataframe 
 
@@ -12,7 +13,8 @@ while keepIn:
     print("Press 1 to download the dataset")
     print("Press 2 to inspect the dataset")
     print("Press 3 to clean the dataset")
-    print("Press 4 to exit")
+    print("Press 4 to connect to Snowflake")
+    print("Press 5 to exit")
     print("Enter your choice: ")
     inputValue = input()
 
@@ -26,6 +28,10 @@ while keepIn:
         df = cleanData.cleanData(df)
 
     elif inputValue == '4':
+        print("Connecting to Snowflake")
+        connectToSnowflake.connectToSnowflake(df)
+
+    elif inputValue == '5':
         keepIn = False
 
 
